c039bcd6c704759efe8041a20527f114a772b9fb
mauryakshitij <kkmaurya2004@gmail.com>
2022-11-12 03:44:30 +0530

    added login pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import MyNavbar from "./components/navbar/navbar";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import AdminLogin from "./components/LoginPage/AdminLogin";
+import UserLogin from "./components/LoginPage/UserLogin";
 
 function App() {
   return (
     <div className="App">
-      <MyNavbar />
+      <Router>
+        <Routes>
+          <Route exact path="/admin/login" element={<AdminLogin />} />
+          <Route exact path="/user/login" element={<UserLogin />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ client/src/components/LoginPage/AdminLogin.js
@@ -0,0 +1,35 @@
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import "./styles.css";
+
+function AdminLogin() {
+  return (
+    
+      <Form className="login-form">
+      <h1>Admin Login</h1>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="type"
+            placeholder="Enter your username"
+            id="email"
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter your password"
+            id="password"
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+
+  );
+}
+
+export default AdminLogin;
