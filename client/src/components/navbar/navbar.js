e3cc346be0d46eb5f28de7737c1bf0ae02d8b77d
mauryakshitij <kkmaurya2004@gmail.com>
2022-11-12 02:14:42 +0530

    first

---
+++ client/src/components/navbar/navbar.js
@@ -0,0 +1,30 @@
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { useNavigate } from "react-router-dom";
+import "./styles.css";
+
+const MyNavbar = () => {
+  let navigate = useNavigate();
+  // const routeChangesignup = () =>{
+  //     let path = `/signup`;
+  //     navigate(path);
+  //   }
+  return (
+    <Navbar expand="lg" variant="dark" bg="dark" className="navbar">
+      <Container>
+        <Navbar.Brand>IITG Hostel</Navbar.Brand>
+        <Nav className="justify-content-end" variant="pills">
+          <Nav.Item>
+            <Nav.Link>Student login</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link>Admin login</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default MyNavbar;
