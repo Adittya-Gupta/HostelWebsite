a58a4ea074490197917153e238a73a288e5e0894
Nova_Adi <95131750+Adittya-Gupta@users.noreply.github.com>
2022-11-12 02:09:37 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -2,14 +2,15 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
-// import userRouter from "./routes/users/user.js"
-// import adminRouter from "./routes/admin/user.js"
+import userRouter from "./routes/users/user.js"
+import adminRouter from "./routes/admin/user.js"
 import dotenv from "dotenv";
 const app = express();
 dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-
+app.use('/users', userRouter);
+app.use('/admin', adminRouter);
 app.use(cors());
 
 const CONNECTION_URL =
