4414136eb908e5055adf7471b269c91950e3ab01
Nova_Adi <95131750+Adittya-Gupta@users.noreply.github.com>
2022-11-12 04:05:53 +0530

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,17 @@ import UserLogin from "./components/LoginPage/UserLogin";
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Router>
         <Routes>
           <Route exact path="/admin/login" element={<AdminLogin />} />
           <Route exact path="/user/login" element={<UserLogin />} />
         </Routes>
       </Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -1,25 +1,26 @@
-import Container from "react-bootstrap/Container";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import { useNavigate } from "react-router-dom";
-import "./styles.css";
-
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import {useNavigate } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 const MyNavbar = () => {
-  return (
+  let navigate = useNavigate(); 
+  return ( 
     <Navbar expand="lg" variant="dark" bg="dark" className="navbar">
-      <Container>
-        <Navbar.Brand>IITG Hostel</Navbar.Brand>
-        <Nav className="justify-content-end" variant="pills">
-          <Nav.Item>
-            <Nav.Link>Student login</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link>Admin login</Nav.Link>
-          </Nav.Item>
-        </Nav>
-      </Container>
-    </Navbar>
-  );
-};
-
+    <Container>
+      <Navbar.Brand>IITG Hostel</Navbar.Brand>
+      <Nav className="justify-content-end" variant="pills">
+        <Nav.Item>
+          <Nav.Link onClick={()=>navigate('/users/login')}>Student login</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link onClick={()=>navigate('/admin/login')}>Admin login</Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </Container>
+  </Navbar>
+   );
+}
+ 
 export default MyNavbar;
