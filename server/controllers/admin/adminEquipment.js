5e1799901d93f28cd8ab52f80a2b381ccac3c23d
Nova_Adi <95131750+Adittya-Gupta@users.noreply.github.com>
2022-11-12 01:34:27 +0530

    commit

---
+++ server/controllers/admin/adminEquipment.js
@@ -0,0 +1,18 @@
+import Inventory from '../../models/inventory';
+
+export const getEquipment = async (req, res) => {
+    try{
+        const inventory = await Inventory.find();
+        res.status(200).json(inventory);
+    }
+    catch(error){
+        res.status(404).json({message: error.message});
+    }
+}
+
+export const editEquipment = async (req, res) => {
+    const {id, name, issued, issued_by, time_issued} = req.body;
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No equipment with id: ${id}`);
+    const updatedEquipment = {name, issued, issued_by, time_issued, _id: id};
+    await Inventory.findByIdAndUpdate(id, updatedEquipment, {new: true}).then();
+}
--- server/routes/admin/admin.js
+++ server/routes/admin/admin.js
@@ -1,5 +1,8 @@
 import Express from 'express';
-import {validateAdmin} from '../../controllers/admin/admin';
+import {validateAdmin} from '../../controllers/admin/adminValidate';
+import { getEquipment, editEquipment } from '../../controllers/admin/adminEquipment';
 const adminRouter = Express.Router();
 
 adminRouter.post('/login', validateAdmin);
+adminRouter.get('/equipment', getEquipment);
+adminRouter.put('/equipment',editEquipment);
